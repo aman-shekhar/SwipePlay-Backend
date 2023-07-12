81ee15adcfc23f57bdbd63868c474a8468cfaef1
aman-shekhar <70257612+aman-shekhar@users.noreply.github.com>
2023-07-12 18:53:44 +0530

    changed port

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use('/users', usersRoutes);
 // app.use('/users', authMiddleware, usersRoutes);
 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.port || 8080;
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
