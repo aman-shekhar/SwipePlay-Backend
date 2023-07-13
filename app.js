f71bcfc2038c2b0be179157246f8af9df0df5b42
aman-shekhar <70257612+aman-shekhar@users.noreply.github.com>
2023-07-13 14:19:13 +0530

    added endpoint for likes

--- app.js
+++ app.js
@@ -13,12 +13,31 @@ app.get('/', (req, res) => {
 
 const challengesData = require('./assets/ChallengesData.json')
 const challengesOrderData = require('./assets/ChallengesOrderData.json')
-app.get('/challenges', (req, res) => {
+const challengesLikesData = require('./assets/ChallengesLikesData.json')
+
+app.get('/challenges/:userId', (req, res) => {
+  const { userId } = req.params;
+  console.log("===userId",userId)
+  //add user in db when curr_data is not true
   req?.query?.curr_data == 'true' ? res.json(challengesOrderData) : res.json(challengesData);
 });
 
-const usersRoutes = require('./routes/users');
-app.use('/users', usersRoutes);
+app.get('/likes', (req, res) => {
+  res.json(challengesLikesData);
+});
+
+app.post('/postLikes/:userId', (req, res) => {
+  const likes = req.body;
+  const { userId } = req.params;
+  //add likes in db
+  console.log("===batch like data ",likes)
+  console.log("====user ",userId)
+  res.sendStatus(200);
+  // let testData = [{ "challengeID": "cricket34", "action": "like" },{"challengeID": "cricket35", "action": "unlike" }]
+});
+
+// const usersRoutes = require('./routes/users');
+// app.use('/users', usersRoutes);
 // app.use('/users', authMiddleware, usersRoutes);
 
 // Start server
